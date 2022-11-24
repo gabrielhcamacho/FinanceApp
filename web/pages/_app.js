3a0868f2fef4cefbdab056df54ff5dc119a0c799
Gabriel Camacho <gabriel.hcamacho@outlook.com>
2022-11-24 09:45:25 -0300

    header upload

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import { ThemeProvider } from 'next-themes'
+import { SessionProvider } from "next-auth/react"
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute='class'>
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp

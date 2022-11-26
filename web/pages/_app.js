99f1956e32f73efcb2a22122cdfad823672ba80a
Gabriel Camacho <gabriel.hcamacho@outlook.com>
2022-11-25 21:09:19 -0300

    Adicionando modal transacao

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,13 +1,16 @@
 import '../styles/globals.css'
 import { ThemeProvider } from 'next-themes'
 import { SessionProvider } from "next-auth/react"
+import { TransactionsProvider } from '../src/contexts/TransactionsContext'
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute='class'>
       <SessionProvider session={pageProps.session}>
-        <Component {...pageProps} />
+        <TransactionsProvider>
+          <Component {...pageProps} />
+        </TransactionsProvider>
       </SessionProvider>
     </ThemeProvider>
   )
